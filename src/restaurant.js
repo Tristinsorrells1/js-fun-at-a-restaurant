ac855b7a48d85884efed0e467031650cba10feb1
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-10-14 18:11:02 -0500

    7 passing

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,31 @@
+///////////////////////////createRestaurant Section///////////////////////
+
+function createRestaurant(name) {
+ var pizzaRestaurant = {
+    name: name,
+    menus: { 
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+  return pizzaRestaurant
+}
+
+///////////////////////////addMenuItem Section///////////////////////
+
+
+
+function addMenuItem(pizzaRestaurant, food) {
+  var x = pizzaRestaurant.menus.lunch
+  x.push(food)
+}
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem
 }
