9210fab98308096e31c8ce40201ead7d2b2e3a76
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-10-14 18:29:59 -0500

    9 passing

--- src/restaurant.js
+++ src/restaurant.js
@@ -14,13 +14,38 @@ function createRestaurant(name) {
 
 ///////////////////////////addMenuItem Section///////////////////////
 
-
-
 function addMenuItem(pizzaRestaurant, food) {
-  var x = pizzaRestaurant.menus.lunch
-  x.push(food)
+  
+  if (food.type === "breakfast" && pizzaRestaurant.menus.breakfast){
+    var y = pizzaRestaurant.menus.breakfast
+    y.push(food)
+  }
+  else if (food.type === "lunch") {
+    var z = pizzaRestaurant.menus.lunch
+    z.push(food)
+  }
+  else {
+    var x = pizzaRestaurant.menus.lunch
+    x.push(food)
+  }
 }
 
+
+// function addMenuItem(pizzaRestaurant, food) {
+
+//   if (food.type === "breakfast") {
+//     var y = pizzaRestaurant.menus.breakfast
+//     y.push(food)
+//   }
+//   else if (food.type === "lunch") {
+//     var z = pizzaRestaurant.menus.lunch
+//     z.push(food)
+//   }
+//   else {
+//     var x = pizzaRestaurant.menus.lunch
+//     x.push(food)
+//   }
+// }
 
 
 
