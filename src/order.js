c29fcc1569ebb8bb7f4781f46abed539b3e3b892
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-10-14 12:43:55 -0500

    7 passing

--- src/order.js
+++ src/order.js
@@ -7,10 +7,23 @@ function takeOrder(order, deliveryOrders) {
 
 ////////////////////////////refundOrder Section///////////////////////
 
+// function refundOrder(orderNum, deliveryOrders2) {
+//   for (var i =0; i < deliveryOrders2.length; i++)
+//     console.log([i])
+//     return deliveryOrders2.shift(deliveryOrders2[i].orderNumber)
+// }
+
 function refundOrder(orderNum, deliveryOrders2) {
-for (var i= 0; i <deliveryOrders2.length; i++)
-  deliveryOrders2.shift(orderNum[i])
+  for (var i = 0; i < deliveryOrders2.length; i++) {
+  console.log(deliveryOrders2[i].orderNumber)
+    if (deliveryOrders2[i].orderNumber === orderNum) {
+       return deliveryOrders2.splice(i, 1)
+      
+  }
 }
+}
+
+
 
 module.exports = {
   takeOrder,
